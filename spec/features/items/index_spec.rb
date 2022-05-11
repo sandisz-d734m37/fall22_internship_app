53807be49a8f2ee5c769dc5f1b43dc00f22311ef
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-10 22:08:40 -0600

    Test: item index page displays all item information

---
+++ spec/features/items/index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe "item index page" do
+  before do
+    @item1 = Item.create!(name: "Item 1", price: 1000, description: "The first item", inventory:10)
+    @item2 = Item.create!(name: "Item 2", price: 2000, description: "The second item", inventory:20)
+    @item3 = Item.create!(name: "Item 3", price: 3000, description: "The third item", inventory:30)
+
+    visit "/items"
+  end
+
+  it "displays a list of items" do
+    expect(page).to have_content("Item 1")
+    expect(page).to have_content("Price: 10.00")
+    expect(page).to have_content("Description: The first item")
+
+    expect(page).to have_content("Item 2")
+    expect(page).to have_content("Price: 20.00")
+    expect(page).to have_content("Description: The second item")
+
+    expect(page).to have_content("Item 3")
+    expect(page).to have_content("Price: 30.00")
+    expect(page).to have_content("Description: The third item")
+  end
+end
