8bc5133e643d21c089705fdae42f205f789b0c8e
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-14 17:50:31 -0600

    Tests: item model method #formatted_price formats price and item index shows items in order

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -2,21 +2,30 @@ require 'rails_helper'
 
 describe "item index page" do
   before do
-    @item1 = Item.create!(name: "Item 1", price: 1000, description: "The first item", inventory:10)
-    @item2 = Item.create!(name: "Item 2", price: 2000, description: "The second item", inventory:20)
-    @item3 = Item.create!(name: "Item 3", price: 3000, description: "The third item", inventory:30)
+    @item1 = Item.create!(name: "B Item 1", price: 1000, description: "The first item", inventory:10)
+    @item2 = Item.create!(name: "C Item 2", price: 2000, description: "The second item", inventory:20)
+    @item3 = Item.create!(name: "A Item 3", price: 3000, description: "The third item", inventory:30)
 
     visit "/items"
   end
 
   it "displays a list of items" do
-    expect(page).to have_content("Item 1")
+    expect(page).to have_content("B Item 1")
     expect(page).to have_content("Price: 10.00")
+    expect(page).to have_content("Inventory: 10")
 
-    expect(page).to have_content("Item 2")
+    expect(page).to have_content("C Item 2")
     expect(page).to have_content("Price: 20.00")
+    expect(page).to have_content("Inventory: 20")
 
-    expect(page).to have_content("Item 3")
+
+    expect(page).to have_content("A Item 3")
     expect(page).to have_content("Price: 30.00")
+    expect(page).to have_content("Inventory: 30")
+  end
+
+  it "displays the items in order" do
+    expect("A Item 3").to appear_before("B Item 1")
+    expect("B Item 1").to appear_before("C Item 2")
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,8 +1,20 @@
 require "rails_helper"
 
 describe Item do
-  context "relationships" do
+  describe "relationships" do
     it{should have_many(:shipment_items)}
     it{should have_many(:shipments).through(:shipment_items)}
+  end
+
+  describe "instance methods" do
+    context "#formatted_price" do
+      it "formats item prices to include all zeroes" do
+        item1 = Item.create!(name: "B Item 1", price: 1000, description: "The first item", inventory:10)
+        item2 = Item.create!(name: "C Item 2", price: 2050, description: "The second item", inventory:20)
+
+        expect(item1.formatted_price).to eq("$10.00")
+        expect(item2.formatted_price).to eq("$20.50")
+      end
+    end
   end
 end
