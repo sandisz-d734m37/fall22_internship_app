a63452d7627abf22c8653e846fc0dc5fe82836aa
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-14 21:22:48 -0600

    Test: if name field is blank new page displays error message

--- spec/features/items/new_spec.rb
+++ spec/features/items/new_spec.rb
@@ -1,9 +1,11 @@
 require 'rails_helper'
 
 describe "New Item page" do
-  it "I see a form to create a new item then ridirects to item show page" do
+  before do
     visit "/items/new"
+  end
 
+  it "Has a form to create a new item then ridirects to item show page" do
     fill_in "Name", with: "Test Item"
     fill_in "Description", with: "Test item's description"
     fill_in "Price", with: 35.50
@@ -15,5 +17,15 @@ describe "New Item page" do
     expect(page).to have_content("Description: Test item's description")
     expect(page).to have_content("Price: $35.50")
     expect(page).to have_content("Inventory: 10")
+  end
+
+  it "Redirects to /items/new and displays error message if name is blank" do
+    fill_in "Description", with: "Failed item's description"
+    fill_in "Price", with: 0.50
+    fill_in "Inventory", with: 1
+
+    click_button "Create Item"
+
+    expect(page).to have_content("The name field cannot be blank")
   end
 end
