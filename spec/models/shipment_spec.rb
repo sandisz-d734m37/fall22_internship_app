f5a942c1da58901b97abde0b955295fc81bc6dd5
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-14 20:02:06 -0600

    Test: shipment#in_or_out determines wether shipment is incoming or outgoing

--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -1,8 +1,22 @@
 require "rails_helper"
 
 describe Shipment do
-  context "relationships" do
+  describe "relationships" do
     it{should have_many(:shipment_items)}
     it{should have_many(:items).through(:shipment_items)}
+  end
+
+  before do
+    @incoming_shipment = Shipment.create!(origin: "123 Fake St", destination: "123 Real Corp Street", outgoing: false, arrived: false, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
+    @outgoing_shipment = Shipment.create!(origin: "123 Real Corp Street", destination: "123 Fake St", outgoing: true, arrived: false, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
+  end
+
+  describe "instance methods" do
+    context "#in_or_out" do
+      it "determines whether shipment is incoming or outgoing" do
+        expect(@incoming_shipment.in_or_out).to eq("Incoming")
+        expect(@outgoing_shipment.in_or_out).to eq("Outgoing")
+      end
+    end
   end
 end
