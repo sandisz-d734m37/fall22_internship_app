32c4429559b5a306fa324768fa2d5135d180a8e7
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-14 23:44:15 -0600

    Test: Shipment#arrival_status returns arrival status

--- spec/models/shipment_spec.rb
+++ spec/models/shipment_spec.rb
@@ -9,7 +9,7 @@ describe Shipment do
   before do
     @incoming_shipment = Shipment.create!(origin: "123 Fake St", destination: "123 Real Corp Street", outgoing: false, arrived: false, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
     @incoming_shipment_2 = Shipment.create!(origin: "123 Fake St", destination: "123 Real Corp Street", outgoing: false, arrived: false, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
-    @outgoing_shipment = Shipment.create!(origin: "123 Real Corp Street", destination: "123 Fake St", outgoing: true, arrived: false, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
+    @outgoing_shipment = Shipment.create!(origin: "123 Real Corp Street", destination: "123 Fake St", outgoing: true, arrived: true, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
     @outgoing_shipment_2 = Shipment.create!(origin: "123 Real Corp Street", destination: "123 Fake St", outgoing: true, arrived: false, created_at: Date.parse("2022-01-01"), updated_at: Date.parse("2022-01-08"))
   end
 
@@ -18,6 +18,13 @@ describe Shipment do
       it "determines whether shipment is incoming or outgoing" do
         expect(@incoming_shipment.in_or_out).to eq("Incoming")
         expect(@outgoing_shipment.in_or_out).to eq("Outgoing")
+      end
+    end
+
+    context "#arrival_status" do
+      it "returns arrival status based on arrival boolean" do
+        expect(@outgoing_shipment.arrival_status).to eq("Arrived")
+        expect(@incoming_shipment.arrival_status).to eq("Still en route")
       end
     end
   end
