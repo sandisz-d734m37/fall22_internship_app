fd86e343a664527744e1c07cf0d69a659018f6c5
Sandisz Thieme <sandiszthieme@gmail.com>
2022-05-15 22:44:58 -0600

    Test: welcome index has links to create new item

---
+++ spec/features/welcome/index_spec.rb
@@ -0,0 +1,27 @@
+require "rails_helper"
+
+describe "Welcome page" do
+  before do
+    visit "/"
+  end
+  
+  it "has a button to create a new item" do
+    click_button("Create a new item")
+
+    expect(current_path).to eq("/items/new")
+  end
+
+  it "has a button to create a new shipment" do
+    click_button("Create a new shipment")
+
+    expect(current_path).to eq("/shipments/new")
+  end
+
+  context "navigation" do
+    it "has links to home page, item index, and shipment index" do
+      expect(page).to have_link("Go to home page")
+      expect(page).to have_link("Go to item index")
+      expect(page).to have_link("Go to shipment index")
+    end
+  end
+end
